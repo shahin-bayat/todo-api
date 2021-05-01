5f4dd8de398c048a93331d887421e24931448662
Shahin Bayat <shahin.de@gmail.com>
2021-05-01 20:00:04 +0430

    change port

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const app = express()
 
 app.get("/", (req, res) => res.send("hello world"))
 
-const PORT = 3000 || process.env.PORT
+const PORT = 3000
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
